75541c40e6f7fbe1ebb43ad26bf156f29d5f9421
Timm <timmschmidt7@t-online.de>
2022-05-03 19:38:55 +0200

    init

---
+++ Learning1/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+
+        int x = 0, y = 1, z = 2;
+        String testStr = "Hello";
+
+        double a = 0.f;
+        x = (int) a;
+
+        System.out.println(testStr.toUpperCase().length());
+    }
+}
